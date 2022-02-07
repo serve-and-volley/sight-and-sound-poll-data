b92ea828cb8e8e67fff0eeebe18d7b35774e9249
Kevin Lin <serve.and.volley@cal.berkeley.edu>
2022-02-07 13:03:41 -0500

    Update create_tables.sql

--- 2012/sql/create_tables.sql
+++ 2012/sql/create_tables.sql
@@ -1,40 +1,46 @@
 CREATE TABLE voters_2012 (
-    voter_id            varchar,    
-    voter_name          varchar,
-    voter_url           varchar,
-    voter_role          varchar,
-    voter_nationality   varchar,
-    voter_gender        varchar,
-    poll_category       varchar,
-    voter_title         varchar,
-    film_count          integer,
-    voter_comments      varchar
+    voter_id                        varchar,
+    voter_name                      varchar,
+    voter_name_searchable           varchar,
+    poll_category                   varchar,
+    voter_role                      varchar,
+    voter_title                     varchar,
+    voter_nationality               varchar,
+    voter_gener                     varchar,
+    film_count                      integer,
+    voter_comments                  varchar
 );
 
 \copy voters_2012 FROM '~/Desktop/voters_2012.csv' DELIMITER ',' CSV;
 
 CREATE TABLE votes_2012 (
-    voter_id            varchar,
-    votes_film_title    varchar,
-    film_url            varchar,
-    film_id             varchar,
-    film_year           integer,
-    film_director       varchar
+    voter_id                        varchar,
+    raw_film_title                  varchar,
+    normed_film_title               varchar,
+    searchable_film_title           varchar,
+    film_director                   varchar,
+    film_director_searchable        varchar,
+    film_year                       integer,
+    film_id                         varchar
 );
 
 \copy votes_2012 FROM '~/Desktop/votes_2012.csv' DELIMITER ',' CSV;
 
 CREATE TABLE films_2012 (
-    film_id             varchar,    
-    films_film_title    varchar,
-    director            varchar,
-    year                integer,
-    country             varchar,
-    genre               varchar,
-    film_type           varchar,
-    category            varchar,
-    actors              varchar,
-    alternate_titles    varchar
+    film_id                         varchar,
+    title_raw                       varchar,
+    title_searchable                varchar,
+    director_raw                    varchar,
+    director_searchable             varchar,
+    year                            integer,
+    country                         varchar,
+    genre                           varchar,
+    media_type                      varchar,
+    content_type                    varchar,
+    actors_raw                      varchar,
+    actors_searchable               varchar,
+    alternate_titles_raw            varchar,
+    alternate_titles_searchable     varchar
 );
 
 \copy films_2012 FROM '~/Desktop/films_2012.csv' DELIMITER ',' CSV;
